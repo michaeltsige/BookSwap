9d67b1cc2eb752312f56ec8ebff66e4de77019b0
michaeltsige <michaeltsige11@gmail.com>
2024-08-30 22:18:33 +0530

    Updated CORS Policy for deployment

--- backend/index.js
+++ backend/index.js
@@ -11,19 +11,16 @@ const app = express();
 // Middleware for parsing request body
 app.use(express.json());
 
-// Middleware for handling CORS POLICY
-// Option 1: Allow All Origins with Default of cors(*)
-app.use(cors());
-// Option 2: Allow Custom Origins
-// app.use(
-//   cors({
-//     origin: 'http://localhost:3000',
-//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//     allowedHeaders: ['Content-Type'],
-//   })
-// );
-
 dotenv.config();
+
+app.use(
+  cors({
+    origin: `${process.env.FRONTEND_URL}`,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type'],
+  })
+);
+
 
 const PORT = process.env.PORT;
 const mongoDBURL = process.env.MONGODB_URL;
