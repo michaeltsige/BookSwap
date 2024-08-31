3df8ba6602decefe8c1e456c223b025e124b777f
michaeltsige <michaeltsige11@gmail.com>
2024-08-31 22:30:46 +0530

    Corrected Book Modal

--- frontend/src/components/home/booksPage/BookModal.jsx
+++ frontend/src/components/home/booksPage/BookModal.jsx
@@ -1,8 +1,19 @@
+import React, { useEffect } from 'react';
 import { AiOutlineClose } from 'react-icons/ai';
 import { PiBookOpenTextLight } from 'react-icons/pi';
 import { BiUserCircle } from 'react-icons/bi';
 
 const BookModal = ({ book, onClose }) => {
+  useEffect(() => {
+    // Disable scrolling when modal is visible
+    document.body.style.overflow = 'hidden';
+
+    // Re-enable scrolling when modal is unmounted
+    return () => {
+      document.body.style.overflow = 'auto';
+    };
+  }, []);
+
   return (
     <div
       className='fixed bg-black bg-opacity-60 top-0 left-0 right-0 bottom-0 z-50 flex justify-center items-center'
